4fe41071290258b5d574c9da0c5fa9adc5737b3c
Tamara <nos.tamaramos@gmail.com>
2015-06-23 22:10:48 -0700

    Skeleton of Bechdel test code

---
+++ projects/bechdel_test.py
@@ -0,0 +1,49 @@
+class BechdelTest():
+
+    male = "M"
+    female = "F"
+
+    def __init__(self, play):
+        self.play = play
+        self._analyze()
+
+    # return dictionary with character name as key,
+    #    gender (M / F) as key
+    def _get_genders(self):
+        # TODO: read in list from play, parse by gender
+        return {}
+
+    # given a node, parse the speaker
+    def _get_speaker(self, speaker_node):
+        # TODO: parse the speaker
+        return ""
+
+    # return a list of names of who is speaking in a given scene
+    def _parse_scenes(self, scene):
+        scene_summary = []
+        actions = scene.childNodes
+        for action in actions:
+            if action.nodeValue != "\n" and action.nodeValue != " \n":
+                try:
+                    if action.tagName == "sp":
+                        speaker = self._get_speaker(action)
+                        scene_summary += [speaker]
+                except AttributeError:
+                    pass # ignore this
+        return scene_summary
+
+    # Step 1: do women talk to each other?
+    def _analyze(self):
+        scenes = self.play.getElementsByTagName("div2")
+        totals = []
+        character_gender_map = self._get_genders()
+        scene_breakdowns = []
+        for scene in scenes:
+            speaker_list = []
+            speakers = self._parse_scenes(scene)
+            for speaker in speakers:
+                if speaker in character_gender_map:
+                    speaker_list.append(character_gender_map[speaker])
+            scene_breakdowns.append(speaker_list)
+
+
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import os
 
 from common.play_data import plays
 from common.models import Play, DataAssembler, FacetedLineChart
-from projects.chars_on_stage import CharsOnStage
+from projects.chars_on_stage import CharsOnStage, BechdelTest
 from projects.play_analyzer import WordAnalyzer
 
 # TODO: test how well DataAssembler works for WordAnalyzer
@@ -14,12 +14,5 @@ for play, play_data in plays.iteritems():
     if i > 0:
         break
     p = Play('data/%s' % play, play_data['full_name'], play_data['genre'])
-    c = CharsOnStage(p)
-    # w = WordAnalyzer(p)
-    charsData.add_data_to_parse(c.data, p.title)
-    i += 1
-
-charsData.create_matrix()
-f = FacetedLineChart(charsData.matrix, "total_words", "total_characters", charsData.play_column_name)
-f.display()
+    b = BechdelTest(p)
 
