03459ec52e2847e7217355b6bbc055ee5878748f
Tamara <nos.tamaramos@gmail.com>
2015-07-25 21:52:38 -0700

    Parsed the speaker

--- projects/bechdel_test.py
+++ projects/bechdel_test.py
@@ -3,6 +3,7 @@ class BechdelTest():
     female = "2"
 
     def __init__(self, play):
+        self.name_map = {}
         self.play = play
 
     # return dictionary with character name as key,
@@ -11,19 +12,21 @@ class BechdelTest():
         gender_map = {}
         char_list = self.play.data.getElementsByTagName("person")
         for char in char_list:
+            xml_id = char.getAttribute("xml:id")
             nameObj = char.getElementsByTagName("name")
             genderObj = char.getElementsByTagName("sex")
             if nameObj and genderObj:
                 if nameObj[0].childNodes:
                     name = nameObj[0].childNodes[0].data
                     gender = genderObj[0].getAttribute("value")
-                    gender_map[name] = gender
+                    gender_map[xml_id] = gender
+                    self.name_map[xml_id] = name
         return gender_map
 
     # given a node, parse the speaker
     def _get_speaker(self, speaker_node):
-        # TODO: parse the speaker
-        return ""
+        speaker_id = speaker_node.getAttribute("who")
+        return speaker_id[1:]
 
     # return a list of names of who is speaking in a given scene
     def _parse_scenes(self, scene):
@@ -47,16 +50,25 @@ class BechdelTest():
 
         ## Part 1: how many female characters do these plays have?
         women = [g == "2" for g in character_gender_map.values()]
-        print self.play.title
-        print women.count(True)
+        if women.count(True) < 2:
+            print "Failed the first part of the Bechdel test: " + self.play.title
+        else:
+            scene_breakdowns = []
+            for scene in scenes:
+                speaker_list = []
+                speakers = self._parse_scenes(scene)
+                for speaker in speakers:
+                    if speaker in character_gender_map:
+                        speaker_list.append(character_gender_map[speaker])
+                scene_breakdowns.append(speaker_list)
 
-        # scene_breakdowns = []
-        # for scene in scenes:
-        #     speaker_list = []
-        #     speakers = self._parse_scenes(scene)
-        #     for speaker in speakers:
-        #         if speaker in character_gender_map:
-        #             speaker_list.append(character_gender_map[speaker])
-        #     scene_breakdowns.append(speaker_list)
+        for scene in scene_breakdowns:
+            women_talking = 0
+            for char_talking in scene:
+                if women_talking == 1 and char_talking == "2":
+                    print "Women talking in a row."
+                if char_talking == "2":
+                    women_talking = 1
+
 
 
--- test.py
+++ test.py
@@ -5,12 +5,12 @@ from projects.chars_on_stage import CharsOnStage
 from projects.bechdel_test import BechdelTest
 from projects.play_analyzer import WordAnalyzer
 
-charsData = DataAssembler(["action_i", "scene_i", "action_type", "total_words", "total_characters"])
-for play, play_data in plays.iteritems():
-    p = Play('data/%s' % play, play_data['full_name'], play_data['genre'])
-    b = BechdelTest(p)
-    b.analyze()
+# charsData = DataAssembler(["action_i", "scene_i", "action_type", "total_words", "total_characters"])
+# for play, play_data in plays.iteritems():
+#     p = Play('data/%s' % play, play_data['full_name'], play_data['genre'])
+#     b = BechdelTest(p)
+#     b.analyze()
 
-# p = Play('data/1H4.xml', 'HenryIV Part I', 'history')
-# b = BechdelTest(p)
-# b.analyze()
+p = Play('data/1H4.xml', 'HenryIV Part I', 'history')
+b = BechdelTest(p)
+b.analyze()
